023cfb884dd4236fd0e589d04818e9b8e462adff
mkusaka <hinoshita1992@gmail.com>
2019-12-08 14:49:31 +0900

    dev: add jest test environments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kits-ts
+
 typescript implementation of [You don't (may not) need Lodash/Underscore](https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_sortby-and-_orderby) examples
---
+++ index.ts
@@ -0,0 +1,8 @@
+// export * from "collection";
+// export * from "util";
+// export * from "array";
+// export * from "function";
+// export * from "number";
+// export * from "lang";
+// export * from "object";
+// export * from "string";
