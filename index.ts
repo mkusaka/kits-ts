14da6c41224d7758b9a29fffda86bab0afd81e50
mkusaka <hinoshita1992@gmail.com>
2019-12-08 15:02:37 +0900

    feat(array): add _.chunk

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
+// to avoid reserved words, add "_" prefix to all import
+import * as _array from 'array';
 // export * from "collection";
 // export * from "util";
-// export * from "array";
 // export * from "function";
 // export * from "number";
 // export * from "lang";
 // export * from "object";
 // export * from "string";
+
+export {
+  _array,
+}
+
+const defaults = {
+  ..._array
+}
+
+export default defaults;
--- packages/array/index.test.ts
+++ packages/array/index.test.ts
@@ -1,5 +1,40 @@
-describe("", () => {
-  test("", () => {
-    expect("").toEqual("");
+import { chunk } from './';
+import * as _ from 'lodash';
+
+describe('chunk', () => {
+  describe('lodash compatibility test', () => {
+    it('one length array equals lodash provided', () => {
+      expect(chunk(['a'], 2)).toEqual(_.chunk(['a'], 2));
+    });
+
+    it('two length array equals lodash provided', () => {
+      expect(chunk(['a', 'b'], 2)).toEqual(_.chunk(['a', 'b'], 2));
+    });
+
+    it('three length array equals lodash provided', () => {
+      expect(chunk(['a', 'b', 'c'], 2)).toEqual(_.chunk(['a', 'b', 'c'], 2));
+    });
+
+    it('chunk with 1 array equals lodash provided', () => {
+      expect(chunk(['a', 'b'], 1)).toEqual(_.chunk(['a', 'b'], 1));
+    });
+  });
+
+  describe('expected result test', () => {
+    it('one length array', () => {
+      expect(chunk(['a'], 2)).toEqual([['a']]);
+    });
+
+    it('two length array', () => {
+      expect(chunk(['a', 'b'], 2)).toEqual([['a', 'b']]);
+    });
+
+    it('three length array', () => {
+      expect(chunk(['a', 'b', 'c'], 2)).toEqual([['a', 'b'], ['c']]);
+    });
+
+    it('chunk with 1 array', () => {
+      expect(chunk(['a', 'b'], 1)).toEqual([['a'], ['b']]);
+    });
   });
 });
